386e3b55af0e17116d4d4c50a1b7037019bc1d94
Alessandro Pilastrini <alessandropilastrini@gmail.com>
2018-08-23 00:25:24 +0200

    installed redux - redux-saga - devtool

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+
+import { reducer } from "./redux";
+import { watcherSaga } from "./sagas";
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+// run the saga
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
